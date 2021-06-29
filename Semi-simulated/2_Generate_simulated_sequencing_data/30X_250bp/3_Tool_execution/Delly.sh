93028f814fc2030e5a1775d11afec872961802f1
AuroUTU <wangningqd@outlook.com>
2021-06-29 10:09:37 +0300

    Delete Semi-simulated/2_Generate_simulated_sequencing_data/30X_250bp directory

--- Semi-simulated/2_Generate_simulated_sequencing_data/30X_250bp/3_Tool_execution/Delly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH -C stable
-#SBATCH --mem 32GB
-#SBATCH --job-name Delly.250
-
-/delly/src/delly call -g RG.2x250.BWA-M.SORTED.SAM.bam -i -o delly_default.bcf
-
-/delly/bcftools/bcftools view delly_default.bcf > delly_default.vcf
--- Semi-simulated/2_Generate_simulated_sequencing_data/30X_250bp/3_Tool_execution/VarScan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH --mem 32GB 
-#SBATCH -C stable
-#SBATCH --job-name VarScan.251
-
-/samtools-1.5/samtools mpileup -B -f chr1.fa RG.2x250.BWA-M.SORTED.SAM.bam > VarScan.short.variants.MPileup
-
-java -jar VarScan.v2.4.3.jar mpileup2indel VarScan.short.variants.MPileup --output-vcf 1 --variants 1 > varscan_indel.vcf
