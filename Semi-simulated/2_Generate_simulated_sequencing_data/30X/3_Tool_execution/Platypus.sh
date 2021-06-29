edb76f8c02961363a00c9fa27fb298555c03b446
AuroUTU <wangningqd@outlook.com>
2021-06-29 10:09:28 +0300

    Delete Semi-simulated/2_Generate_simulated_sequencing_data/30X directory

--- Semi-simulated/2_Generate_simulated_sequencing_data/30X/3_Tool_execution/Platypus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH --partition normal 
-#SBATCH --job-name Platypus.30.A
-#SBATCH --error Platypus.30.A.Errors.txt
-#SBATCH --output Platypus.30.A.Console.txt
-
-python /Platypus/bin/Platypus.py callVariants --bamFiles=RG.30X.BWA-M.SORTED.SAM.bam --refFile=chr1.fa --output=variants.assembel.vcf --assemble=1 --genSNPs=0 --maxSize=6100 --nCPU=24
-
