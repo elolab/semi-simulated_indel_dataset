a553d59aea13bb4e9a992573a93fc7e84069faf3
AuroUTU <wangningqd@outlook.com>
2021-06-30 10:17:40 +0300

    Delete CHM1 directory

--- CHM1/Delly/DELLY.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition long 
-#SBATCH --job-name Delly
-
-/delly/src/delly call -g genome.fa CHM1.SAM.SORTED.BAM -i -o delly_WGS.bcf
-
-/delly/bcftools/bcftools view delly_WGS.bcf > delly_WGS.vcf
