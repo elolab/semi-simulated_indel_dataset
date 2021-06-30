91e6cdab4fc7f2eff5bce9c853eb38e5735bbffa
AuroUTU <wangningqd@outlook.com>
2021-06-30 12:03:29 +0300

    Delete Filter.R

--- exome/Platypus/Filter.R
+++
@@ -1,4 +0,0 @@
-Platypus_input <- read.table("variants.vcf")
-Platypus_input <- Platypus_input[nchar(as.character(Platypus_input[,5]))>=50 |nchar(as.character(Platypus_input[,4]))>=50,]
-Platypus_ins <- Platypus_input[nchar(as.character(Platypus_input[,5]))-nchar(as.character(Platypus_input[,4]))>=1,]
-Platypus_del <- Platypus_input[nchar(as.character(Platypus_input[,4]))-nchar(as.character(Platypus_input[,5]))>=1,]
