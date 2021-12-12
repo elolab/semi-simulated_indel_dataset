42b1081644fe06dcb74dd05028aaa64be05a8440
AuroUTU <wangningqd@outlook.com>
2021-12-12 23:17:25 +0200

    Add files via upload

---
+++ Semi-simulated/1_Generate_simulated_genome/chr2/3.variants_normalization/Combine.R
@@ -0,0 +1,32 @@
+haplotype_1 <- read.table("chr2.haplotype_1.hg19.txt")
+haplotype_2 <- read.table("chr2.haplotype_2.hg19.txt")
+
+homo <- haplotype_1[haplotype_1[,5]=="1/1",]
+heter_1 <- haplotype_1[haplotype_1[,5]=="1/0",]
+heter_2 <- haplotype_2[haplotype_2[,5]=="0/1",]
+
+variants <- rbind(homo,heter_1,heter_2)
+variants <- variants[order(variants[,2]),]
+rownames(variants) <- seq(length=nrow(variants))
+write.table(variants,"Venter.truthset.chr2.txt",row.names = FALSE,col.names = FALSE,quote = FALSE, sep = "\t")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Semi-simulated/1_Generate_simulated_genome/chr2/3.variants_normalization/Convert.confused.herter.R
@@ -0,0 +1,48 @@
+truthset_ground <- read.table("Venter.truthset.left.align.chr2.txt")
+colnames(truthset_ground)<- c("Type","Left.position","Sequence","OP","Genotype")
+#new.position <- as.numeric(as.character(truthset_ground[,2]))#-as.numeric(as.character(truthset_ground[,3]))
+#truthset_ground <- cbind(truthset_ground,new.position)
+#truthset_ground[truthset_ground[,7]=="1/0",7] <- "0/1"
+#truthset.deletion <- truthset_ground[truthset_ground[,5]=="deletion",]
+#truthset.insertion <- truthset_ground[truthset_ground[,5]=="insertion",]
+
+d <- truthset_ground[duplicated(truthset_ground[,2]),]
+remove <- rownames(d)
+truthset <- truthset_ground[!rownames(truthset_ground) %in% remove,]
+rownames(truthset) <- seq(length=nrow(truthset))
+# length <- nchar(as.character(d[,3]))
+# duplication <- cbind(d,length)
+# 
+# # safety enough to delete all of them
+# summary(length)
+# table(duplication[,5])
+
+length <- nchar(as.character(truthset[,3]))
+truthset <- cbind(truthset,length)
+wried_truthset <- vector()
+for (i in 1:(dim(truthset)[1]-1))
+{
+  print(i)
+  position_1 <- as.numeric(substr(as.character(truthset[i,4]),4,nchar(as.character(truthset[i,4]))))
+  position_2 <- as.numeric(substr(as.character(truthset[i,4]),4,nchar(as.character(truthset[i,4]))))+as.numeric(truthset[i,6])
+  position_3 <- as.numeric(as.character(truthset[i+1,2]))
+  if ((position_3-position_2)<=1 & truthset[i,1]=="deletion")
+  {
+    wried_truthset <- rbind(wried_truthset,truthset[i,])
+    wried_truthset <- rbind(wried_truthset,truthset[i+1,])
+  }
+  else if ((position_3-position_1)<=1 & truthset[i,1]=="insertion")
+  {
+    wried_truthset <- rbind(wried_truthset,truthset[i,])
+    wried_truthset <- rbind(wried_truthset,truthset[i+1,])
+  }
+}
+wried_truthset <- wried_truthset[!duplicated(wried_truthset), ]
+truthset <- truthset[!(truthset[,2] %in% wried_truthset[,2]),]
+truthset_for_analysis <- truthset[,-6]
+write.table(truthset_for_analysis,"Venter_truthset_for_analysis.txt",row.names = FALSE,col.names = FALSE,quote = FALSE, sep = "\t")
+
+
+
+
+
