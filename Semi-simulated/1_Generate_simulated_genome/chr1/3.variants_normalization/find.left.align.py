4c58a464e7d267086ee62950d3e9161b39c1c4ee
AuroUTU <wangningqd@outlook.com>
2021-12-12 23:11:46 +0200

    Update find.left.align.py

--- Semi-simulated/1_Generate_simulated_genome/chr1/3.variants_normalization/find.left.align.py
+++ Semi-simulated/1_Generate_simulated_genome/chr1/3.variants_normalization/find.left.align.py
@@ -1,16 +1,15 @@
-reference = open("/home/kning/Bio/Indels_calling/mfranberg_simulated/Venter/chr1/svsim_haplotypes/Repeat/left-align/left.align.truthset/Prove.which.is.correct.truth.set/chr1.fa","r")
+reference = open("chr1.fa","r")
 referenceline = reference.readlines()
 global b
 seq=""
-for j in referenceline[1:]: # get rid of ">chr1"
-	a = j.replace("\n","") # put all the lines into one sequence
+for j in referenceline[1:]: 
+	a = j.replace("\n","") 
 	seq=seq+a
 b = seq
 
 def cutting_right(start_C,pattern_C,length_C):
-	print "C"
 	variants_C = pattern_C.upper()
-	suffix_coordinate = len(pattern_C)-1 # last letter of variants
+	suffix_coordinate = len(pattern_C)-1 
 	print variants_C
 	suffix = pattern_C[suffix_coordinate].upper()
 	flank_left_coordinate = start_C-1	 
@@ -20,26 +19,19 @@ def cutting_right(start_C,pattern_C,length_C):
 	while suffix == flank_left:
 		variants_pattern = variants_pattern[len(variants_pattern)-1]+variants_pattern[:len(variants_pattern)-1]
 		print variants_pattern
-		#print variants_pattern
 		if suffix_coordinate == 0:
 			suffix_coordinate = len(pattern_C)
 		suffix_coordinate = suffix_coordinate-1
 		flank_left_coordinate = flank_left_coordinate-1
-		print flank_left_coordinate
 		suffix =variants_C[suffix_coordinate].upper()
-		print suffix
-		flank_left = b[flank_left_coordinate].upper()
-		print flank_left		
+		flank_left = b[flank_left_coordinate].upper()		
 		
-	flank_left_coordinate = flank_left_coordinate+1 # new start, python 0-base +1 and deleted trings must include the base before the event -1 are neutralized
-	print flank_left_coordinate,variants_pattern
-
+	flank_left_coordinate = flank_left_coordinate+1 
 	return flank_left_coordinate, variants_pattern, length_C
 
 bedA = open("Venter.truthset.txt","r")
 fileA = open("Venter.truthset.left.align.txt","a")
 bedlineA = bedA.readlines()
-#file_distance = open("distance.txt","a")
 for i in range(0,43336):  
 	item = bedlineA[i].split("\t")
 	start = int(item[1]) # python & svsim zero-based, reference 1-based
@@ -59,45 +51,3 @@ for i in range(0,43336):
 bedA.close()
 fileA.close()
 reference.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
