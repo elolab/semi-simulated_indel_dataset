0c8cc962cf81c2a893aee4aa6b90852324014c03
AuroUTU <wangningqd@outlook.com>
2021-12-12 23:13:36 +0200

    Update calculate.gap.py

--- Semi-simulated/1_Generate_simulated_genome/chr1/4.svsim/calculate.gap.py
+++ Semi-simulated/1_Generate_simulated_genome/chr1/4.svsim/calculate.gap.py
@@ -1,6 +1,6 @@
 Haplotype_1 = open("Haplotype_1.fa","r")
 Haplotype_1_no_gap = open("Haplotype_1_no_gap_chr1.fa","w")
-data = Haplotype_1.read().replace('N', '') # the first 5 is ">chr1"
+data = Haplotype_1.read().replace('N', '') 
 Haplotype_1_no_gap.write(data)
 Haplotype_1.close()
 Haplotype_1_no_gap.close()
@@ -11,5 +11,3 @@ data2 = Haplotype_2.read().replace('N', '')
 Haplotype_2_no_gap.write(data2)
 Haplotype_2.close()
 Haplotype_2_no_gap.close()
-
-#rsync -v -a --bwlimit=30000 /home/kning/Bio/Indels_calling/mfranberg_simulated/Venter ningwang@head02.btk.fi:/elo/genomics/netapp/B18010_SV_genome
