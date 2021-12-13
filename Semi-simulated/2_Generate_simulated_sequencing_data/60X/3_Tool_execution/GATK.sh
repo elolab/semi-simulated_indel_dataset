af87b7fd7095debe3871bd2cbacbcb942bd4974e
AuroUTU <wangningqd@outlook.com>
2021-12-13 13:11:20 +0200

    Delete Semi-simulated/2_Generate_simulated_sequencing_data/60X/3_Tool_execution directory

--- Semi-simulated/2_Generate_simulated_sequencing_data/60X/3_Tool_execution/GATK.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition normal
-#SBATCH --job-name GATK.60
-#SBATCH --error GATK.60.Errors.txt
-#SBATCH --output GATK.60.Console.txt
-
-/gatk-4.0.1.2/gatk HaplotypeCaller -R chr1.fa -I RG.60X.BWA-M.SORTED.SAM.bam -O GATK_R.vcf  
-
--- Semi-simulated/2_Generate_simulated_sequencing_data/60X/3_Tool_execution/Platypus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH --partition normal 
-#SBATCH --job-name Platypus.60
-#SBATCH --error Platypus.60.Errors.txt
-#SBATCH --output Platypus.60.Console.txt
-
-python /Platypus/bin/Platypus.py callVariants --bamFiles=RG.60X.BWA-M.SORTED.SAM.bam --refFile=chr1.fa --output=variants.vcf --genSNPs=0 --maxSize=6100 --nCPU=24
-
