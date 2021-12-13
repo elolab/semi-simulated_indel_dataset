91547663c6bd876c98ba5d5156dbc0ed474c1c8c
AuroUTU <wangningqd@outlook.com>
2021-12-13 13:13:53 +0200

    Delete Semi-simulated/2_Generate_simulated_sequencing_data/30X_250bp/3_Tool_execution directory

--- Semi-simulated/2_Generate_simulated_sequencing_data/30X_250bp/3_Tool_execution/Strelka2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH --mem 32GB
-#SBATCH -C stable
-#SBATCH --job-name Strelka.251
-
-/strelka2/strelka-2.9.2.centos6_x86_64/bin/configureStrelkaGermlineWorkflow.py --bam RG.2x250.BWA-M.SORTED.SAM.bam --ref chr1.fa --runDir Venter_chr1 
-
-Venter_chr1/runWorkflow.py -m local -j 24
