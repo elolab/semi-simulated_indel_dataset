cfd217ceaf643adce6c82bfe6ba996b30d8eef10
AuroUTU <wangningqd@outlook.com>
2021-12-23 14:01:48 +0200

    Add files via upload

---
+++ Semi-simulated/2_Generate_simulated_sequencing_data/5X/4_Tool_results_exact/GATK/results.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --cpus-per-task 1
+#SBATCH --error results.Error.txt
+#SBATCH --output results.Output.txt
+#SBATCH --partition normal
+#SBATCH --time 12:00:00
+#SBATCH --job-name GATK_R
+
+module add R
+Rscript parse_GATK.R
